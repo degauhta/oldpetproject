6b21e78fb35bc409d2f14964b16136711460c6ff
Denis Gagarsky <dega.uhta@gmail.com>
2022-09-21 13:17:53 +0300

    add toolbar

--- app/src/main/java/old/pet/project/ui/RootActivity.kt
+++ app/src/main/java/old/pet/project/ui/RootActivity.kt
@@ -2,11 +2,30 @@ package old.pet.project.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_root.toolbar
 import old.pet.project.R
+import old.pet.project.extensions.dpToIntPx
 
 class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val logo = if (toolbar.childCount > 2) toolbar.getChildAt(2) as ImageView else null
+        logo?.scaleType = ImageView.ScaleType.CENTER_CROP
+        val lp = logo?.layoutParams as? Toolbar.LayoutParams
+        lp?.let {
+            it.width = this.dpToIntPx(40)
+            it.height = this.dpToIntPx(40)
+            it.marginEnd = this.dpToIntPx(16)
+            logo.layoutParams = it
+        }
     }
 }
