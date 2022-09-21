de15701390b4e2758d15b655fc06d20ce3c0be70
Denis Gagarsky <dega.uhta@gmail.com>
2022-09-21 10:25:00 +0300

    add structure

---
+++ app/src/main/java/old/pet/project/ui/RootActivity.kt
@@ -0,0 +1,12 @@
+package old.pet.project.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import old.pet.project.R
+
+class RootActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root)
+    }
+}
